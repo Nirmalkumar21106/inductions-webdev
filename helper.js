54c2031d4eb80941f3f320ea97fb030e6642d539
Nirmalkumar21106 <75196397+Nirmalkumar21106@users.noreply.github.com>
2021-06-03 12:48:08 +0530

    Add files via upload

---
+++ helper.js
@@ -0,0 +1,6 @@
+var first = document.getElementsByClassName("inside");
+console.log(first);
+var inner = document.getElementsByClassName("middle")
+console.log(inner)
+var individual = document.querySelector("#unknown")
+console.log(individual)
